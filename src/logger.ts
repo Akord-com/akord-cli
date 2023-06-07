fe0426f709255141001b54285760df06c3d7f61f
jarrvis <michal.kaliszewski@pl.bosch.com>
2023-06-07 11:02:18 +0200

    feat: add log file config; redact secrets

--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,16 @@
 import os from 'os';
 import ora from 'ora';
 import { format, createLogger, transports } from "winston";
+import makeRedact from 'redact-secrets'
+
 const { combine, timestamp, prettyPrint } = format;
 const LOGS_PATH = `${os.homedir()}/.akord/logs`
-
+const redact = makeRedact('[REDACTED]')
 
 export const logger = createLogger({
     level: "debug",
     format: combine(
+        format(info => redact.map(info))(),
         timestamp({
             format: "MMM-DD-YYYY HH:mm:ss",
         }),
@@ -16,10 +19,14 @@ export const logger = createLogger({
     transports: [
         new transports.File({
             filename: LOGS_PATH,
+            maxsize: 5000000, 
+            maxFiles: 10
         }),
         new transports.File({
             level: "error",
             filename: LOGS_PATH,
+            maxsize: 5000000, 
+            maxFiles: 10 
         }),
     ],
 });
